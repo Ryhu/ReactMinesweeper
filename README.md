2821bd1e73269151c5e9c24089d774fe7fbff5fe
Ryan Hu <ryanhuuu@gmail.com>
2018-08-27 13:42:44 -0400

    grid now generates correct mined array

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ board for the entire thing,
 sizeInput for the input of height, width, Mines
 Grid for actual clickable game board,
 Tile for the clickable tiles
+
+first up is the input, make the input send data to the Grid component
+
+
+create basic tile, has size, background color, borders
+next step is to add functionality, try to multiply tiles by grid data
+before that, an array has to be generated.
+
+to save processing power, the array will be generated before the render is done.
+mines have to be put on the board, and then the numbers are added based on the mines.
+
+for randomness, upon array creation, each square will have a  ((# of mines) / (board size)) chance of being a mine.
+a counter will increment with the creation process.
+at the end, if counter >  mines, then all mine squares will be gathered, put into an array, shuffled, and extra mines will be poped out of the mines array. those coordinates will then be changed to empties
+
+if counter < mines, then all empties will be arrayed, shuffled, and excess will be popped out, and changed.
+
+After the mine laying process, the array will be looped through and numbered based on surrounding mines.
+
+Only after the array is done will it be rendered.
+
+...vs brute force
+
+make 1d array, height x width long. first x slots are mines, x =  # of mines. array is then shuffled and cut into 2x array
+shuffle = fisher-yates
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,19 +7,86 @@ class Grid extends Component {
     super(props)
 
     this.state={
-      height: 0,
-      width: 0,
-      mines: 0,
+      height: 5,
+      width: 5,
+      mines: 14,
+      grid: []
     }
   }
 
+  shouldComponentUpdate(){
+    this.gridMaker()
+  }
+
+  gridMaker(){
+    let workingArray = [] //array to work on
+    let result = [] //array to push to state
+    let empties = (this.state.height * this.state.width) - this.state.mines //empty squares
+    //makes mines
+    for(let i = 0; i < this.state.mines; i++){
+      workingArray.push("*")
+    }
+    //makes empties
+    for(let i = 0; i < empties; i++){
+      workingArray.push(" ")
+    }
+    //shuffle board, mixes mines and empties
+    workingArray = this.shuffle(workingArray)
+
+    for(let i = 0; i < this.state.height; i++){
+      let curVal = (this.state.height * (i+1))
+      let temp = workingArray.slice(curVal - this.state.height, curVal)
+      result.push(temp)
+    }
+
+    console.log(result)
+  }
+
+
+
+  //copypasta'd
+  shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
+  // rowMaker(y){
+  //   let result = []
+  //   for(let x = 0; x < this.state.width; x++){
+  //     result.push(this.mineGenerator())
+  //   }
+  //   return result
+  // }
+  //
+  // mineGenerator(){
+  //   let size = this.state.height * this.state.width
+  //   let chance = this.state.mines / size //probability is a spelling liability
+  //   if (Math.random() < chance){
+  //     return "*"
+  //   }
+  //   else{
+  //     return " "
+  //   }
+  // }
+
+
 
   render() {
-    return (
-      <div className="Grid">
-        <Tile />
-      </div>
-    );
+    this.gridMaker()
+    return <p>hi</p>
   }
 }
 
